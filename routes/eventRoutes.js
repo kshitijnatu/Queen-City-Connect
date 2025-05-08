a4909c6d616335370fe9d838b7951f4011f3244e
kshitijnatu <kshitij.natu@gmail.com>
2025-05-08 17:17:45 -0400

    Modified eventRoutes.js to add validation to the events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,7 +4,7 @@ const eventController = require('../controllers/eventController');
 const multer = require('multer');
 const path = require('path');
 const { isLoggedIn, isAuthor } = require('../middlewares/auth');
-const {validateId} = require('../middlewares/validator');
+const { validateId, validateEvent, validateRSVP, validateResult } = require('../middlewares/validator');
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
@@ -18,7 +18,6 @@ const storage = multer.diskStorage({
             const currentDate = new Date().toISOString().split('T')[0];
             const fileName = file.originalname.split('.')[0];
             const customFileName = `${userName}-${fileName}-${currentDate}`;
-            // cb(null, Date.now() + path.extname(file.originalname));
             cb(null, customFileName + path.extname(file.originalname));
         } catch (err) {
             cb(err);
@@ -28,28 +27,30 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-
 const router = express.Router();
 
 // GET /events: send the list of events
 router.get('/', eventController.index);
 
-// GET /events/new: send html form for creating a new event
+// GET /events/new: send HTML form for creating a new event
 router.get('/newEvent', isLoggedIn, eventController.new);
 
 // POST /events: create a new event
-router.post('/', isLoggedIn, upload.single('image'), eventController.create);
+router.post('/', isLoggedIn, upload.single('image'), validateEvent, validateResult, eventController.create);
 
 // GET /events/:id: send details of event identified by id
 router.get('/:id', validateId, eventController.show);
 
-// GET /events/:id/edit: send html form for editing an existing event
+// GET /events/:id/edit: send HTML form for editing an existing event
 router.get('/:id/edit', isLoggedIn, validateId, isAuthor, eventController.edit);
 
 // PUT /events/:id: update the event identified by id
-router.put('/:id', isLoggedIn, validateId, isAuthor, upload.single('image'), eventController.update);
+router.put('/:id', isLoggedIn, validateId, isAuthor, upload.single('image'), validateEvent, validateResult, eventController.update);
 
 // DELETE /events/:id: delete the event identified by id
 router.delete('/:id', isLoggedIn, validateId, isAuthor, eventController.delete);
+
+// POST /events/:id/rsvp: handle RSVP logic
+router.post('/:id/rsvp', isLoggedIn, validateId, validateRSVP, validateResult, eventController.rsvp);
 
 module.exports = router;
